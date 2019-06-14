234a1f4641494c72c0074a205c6904e46b94e06f
Adrian Evangelista <adrianevangelista.bicolresearch@gmail.com>
2019-06-14 14:51:49 +0800

    Placed the close button of Feed detail screen at the bottom.
    Placed the close button of Feed detail screen at the bottom.
    The botton stays on the screen always .

--- lib/src/screens/feeds_screens/feed_detail_screen.dart
+++ lib/src/screens/feeds_screens/feed_detail_screen.dart
@@ -20,56 +20,66 @@ class FeedDetailScreen extends StatelessWidget{
   Widget build(BuildContext context) {
     return SafeArea(      
       child: Scaffold( 
-        body: SingleChildScrollView(
-            child: Container(
-            padding: EdgeInsets.only(left:30, right: 30, bottom: 50.0),
-              child: Column(
-            children: <Widget>[ 
-              Container(
-                padding: EdgeInsets.only(top: 30.0),
-                child: Container(   
-                  //width: 200.0,  
-                  height: 40.0,        
-                  decoration: BoxDecoration(
-                    color: Colors.brown[600],
-                  border: Border.all(width: 0.8),
-                  borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("Mount Carmel Church now a National Shrine",
-                    style: TextStyle(color: Colors.white,
-                    fontFamily: "Helvetica"),
-                    textAlign: TextAlign.center,
+        body: Column(
+          children: <Widget>[
+            Expanded(
+              child: SingleChildScrollView(
+                  child: Container(
+                  padding: EdgeInsets.only(left:30, right: 30, bottom: 50.0),
+                    child: Column(
+                      children: <Widget>[ 
+                        Container(
+                          padding: EdgeInsets.only(top: 30.0),
+                          child: Container(   
+                          height: 40.0,        
+                          decoration: BoxDecoration(
+                            color: Colors.brown[600],
+                            border: Border.all(width: 0.8),
+                          borderRadius: BorderRadius.circular(10.0),
+                          ),
+                        child: Padding(
+                          padding: const EdgeInsets.all(8.0),
+                          child: Text("Mount Carmel Church now a National Shrine",
+                          style: TextStyle(color: Colors.white,
+                          fontFamily: "Helvetica"),
+                          textAlign: TextAlign.center,
+                          ),
+                        ),
+                      ),
                     ),
-                  ),
+                    Container(
+                      padding: EdgeInsets.all(20),
+                      width: double.infinity, 
+                      height: 300, 
+                      child: Image.network(AppConstants.FEEDS_API_BASE_URL+feed.cover_photo),
+                      ),
+                      Text( feed.content ,
+                      style: AppConstants.OPTION_STYLE1 ,
+                      textAlign: TextAlign.justify, 
+                      ),
+                    ],          
                   ),
                 ),
-              Container(
-            padding: EdgeInsets.all(20),
-            width: double.infinity, 
-            height: 300, 
-            child: Image.network(AppConstants.FEEDS_API_BASE_URL+feed.cover_photo),
+              ),
             ),
-          Text( feed.content,
-            style: AppConstants.OPTION_STYLE1 ,
-            textAlign: TextAlign.justify, 
-            ),
-            RaisedButton(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(20.0)
-            ),
-            color: Colors.brown,
-            child: Text("Close", 
-            style: TextStyle(color: Colors.white),
-            ),
-            onPressed: () {
-              Navigator.pop(context);
-            },
+
+            Container(
+              alignment: Alignment.bottomCenter,
+              margin: EdgeInsets.only(top: 10.0, bottom: 30.0),
+              child: RaisedButton(
+              shape: RoundedRectangleBorder(
+                borderRadius: BorderRadius.circular(20.0)
+              ),
+              color: Colors.brown,
+              child: Text("Close", 
+              style: TextStyle(color: Colors.white),
+              ),
+              onPressed: () {
+                Navigator.pop(context);
+              },
           ),
-              ],          
             ),
-            ),
+          ],
         ),
       ),
     );
