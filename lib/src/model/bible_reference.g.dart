f0782b5783e9e04167d76f140232d59b2cafe09e
Adrian Evangelista <adrianevangelista.bicolresearch@gmail.com>
2019-06-18 14:13:12 +0800

    Added Bible Reference model. Adde class for Bible screen. Added GenesisBook class.

---
+++ lib/src/model/bible_reference.g.dart
@@ -0,0 +1,47 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'bible_reference.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+BibleReference _$BibleReferenceFromJson(Map<String, dynamic> json) {
+  return BibleReference(
+      json['reference'] as String,
+      (json['verses'] as List)
+          ?.map((e) =>
+              e == null ? null : Verse.fromJson(e as Map<String, dynamic>))
+          ?.toList(),
+      json['text'] as String,
+      json['translation_id'] as String,
+      json['translation_name'] as String,
+      json['translation_note'] as String);
+}
+
+Map<String, dynamic> _$BibleReferenceToJson(BibleReference instance) =>
+    <String, dynamic>{
+      'reference': instance.reference,
+      'verses': instance.verses,
+      'text': instance.text,
+      'translation_name': instance.translation_name,
+      'translation_id': instance.translation_id,
+      'translation_note': instance.translation_note
+    };
+
+Verse _$VerseFromJson(Map<String, dynamic> json) {
+  return Verse(
+      json['verse'] as String,
+      json['book_id'] as String,
+      json['book_name'] as String,
+      json['chapter'] as String,
+      json['text'] as String);
+}
+
+Map<String, dynamic> _$VerseToJson(Verse instance) => <String, dynamic>{
+      'book_id': instance.book_id,
+      'book_name': instance.book_name,
+      'chapter': instance.chapter,
+      'verse': instance.verse,
+      'text': instance.text
+    };
---
+++ lib/src/screens/profile_screens/bible_screen.dart
@@ -0,0 +1,95 @@
+/*
+*	Filename		:	bible_screen.dart
+*	Purpose			:	displays the list of bible books
+* Created			: 2019-06-18 10:02:34 by Detective Conan
+*	Updated			: 2019-06-18 10:02:34 by Detective Conan 
+*	Changes			:
+*/
+
+import 'package:flutter/material.dart';
+import 'package:mt_carmel_app/src/constants/app_constants.dart';
+
+import 'package:mt_carmel_app/src/helpers/bible_helpers/bible_book.dart';
+import 'package:mt_carmel_app/src/helpers/bible_helpers/bible_books.dart';
+
+import 'package:mt_carmel_app/src/widgets/left_arrow_back_button.dart';
+
+
+class BibleScreen extends StatefulWidget {
+
+  static const TextStyle optionStyle = TextStyle(fontSize: 30, fontWeight: FontWeight.bold);
+
+  BibleScreen(BuildContext context);
+
+  @override
+  _BibleScreenState createState() => _BibleScreenState();
+}
+
+class _BibleScreenState extends State<BibleScreen> {
+
+  BibleBooks _bibleBooks;
+
+  @override
+  void initState() {  
+    _bibleBooks = BibleBooks();
+    super.initState();
+  }  
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      resizeToAvoidBottomPadding: false,
+      body: Container(
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: const EdgeInsets.all(8.0),
+              child: Text("Old Testaments",
+                style: AppConstants.OPTION_STYLE3,
+                textAlign: TextAlign.center,
+              ),
+            ),
+            Expanded(
+              child: SingleChildScrollView(
+                child: Container(
+                  height: MediaQuery.of(context).size.height - 200.0,
+                  child: _bibleBooks.oldTestamentBooks.isNotEmpty
+                      ?GridView.count(
+                        primary: true,
+                        crossAxisCount: 6,
+                        children: List.generate(_bibleBooks.oldTestamentBooks.length, (index) {
+                          return getStructuredGridCell(_bibleBooks.oldTestamentBooks[index]);
+                        }   
+                      ),
+                    ):LinearProgressIndicator(),
+                ),
+              ),
+            ),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: leftArrowBackButton(context: context),
+          ),
+          ],
+        ),
+      )        
+    );
+  }
+
+Card getStructuredGridCell(BibleBook bibleBook) {
+    return Card(
+      elevation: 1.5,
+      child: Center(
+        child: Padding(
+          padding: EdgeInsets.fromLTRB(5.0, 5.0, 5.0,5.0),
+          child: Text(bibleBook.bookName, style: AppConstants.OPTION_STYLE2, 
+            textAlign: TextAlign.center,),
+        ),
+      )
+    );
+  }
+  
+  void close(){    
+    this.dispose();
+  }    
+}
