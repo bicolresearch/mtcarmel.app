476536b15838216e80962ee6685a60ad599c972c
Adrian Evangelista <adriane.macer@gmail.com>
2019-06-26 20:03:22 +0800

    Added History content in about screen.

--- lib/src/screens/profile_screens/about_screen.dart
+++ lib/src/screens/profile_screens/about_screen.dart
@@ -7,6 +7,7 @@
 */
 
 import 'package:flutter/material.dart';
+import 'package:flutter_html_textview_render/html_text_view.dart';
 import 'package:mt_carmel_app/src/constants/app_constants.dart';
 
 import 'package:http/http.dart' as http;
@@ -17,13 +18,14 @@ import 'package:mt_carmel_app/src/model/about.dart';
 import 'package:mt_carmel_app/src/presentations/mount_carmel_icons.dart';
 import 'package:mt_carmel_app/src/widgets/left_arrow_back_button.dart';
 import 'package:mt_carmel_app/src/widgets/line.dart';
+
 ////
 class AboutScreen extends StatefulWidget {
-
   static const String dateOfEstablishmentLabel = "Date of Establishment";
   static const String feastDayLabel = "Feast day";
   static const String titularLabel = "Titular";
   static const String dioceseLabel = "Diocese";
+  static const String historyLabel = "History";
 
   //////
   const AboutScreen({Key key}) : super(key: key);
@@ -33,7 +35,6 @@ class AboutScreen extends StatefulWidget {
 }
 
 class _AboutScreenState extends State<AboutScreen> {
-
   List<About> _aboutList = [];
 
   final List<_AboutItem> _aboutItems = [];
@@ -43,121 +44,132 @@ class _AboutScreenState extends State<AboutScreen> {
   var _isJsonFailed = false;
 
   @override
-  void initState() { 
-    super.initState();   
+  void initState() {
+    super.initState();
     this.getJasonData();
   }
 
-  Future<void> getJasonData() async{
-    var response = await http.get(AppConstants.ABOUT_JSON_URL); 
-    if(this.mounted){
-      setState(() 
-        {
-          if (response.statusCode == 200) {
-              _aboutList = (json.decode(response.body) as List)
-            .map((data) => new About.fromJson(data))
-            .toList();
-            // get only the first in the list
-            About about = _aboutList[0];
+  Future<void> getJasonData() async {
+    var response = await http.get(AppConstants.ABOUT_JSON_URL);
+    if (this.mounted) {
+      setState(() {
+        if (response.statusCode == 200) {
+          _aboutList = (json.decode(response.body) as List)
+              .map((data) => new About.fromJson(data))
+              .toList();
+          // get only the first in the list
+          About about = _aboutList[0];
 
-            if(_aboutList.isNotEmpty){
-              _aboutItems.add(_AboutItem(about, AboutScreen.dateOfEstablishmentLabel));
-              _aboutItems.add(_AboutItem(about, AboutScreen.feastDayLabel));
-              _aboutItems.add(_AboutItem(about, AboutScreen.titularLabel));
-              _aboutItems.add(_AboutItem(about, AboutScreen.dioceseLabel));
-
-            }
-            _isLoading = false;              
-          } 
-          else 
-          {
-            print(response.statusCode);
-            _isJsonFailed = true;
-            _isLoading = false;
+          if (_aboutList.isNotEmpty) {
+            _aboutItems
+                .add(_AboutItem(about, AboutScreen.dateOfEstablishmentLabel));
+            _aboutItems.add(_AboutItem(about, AboutScreen.feastDayLabel));
+            _aboutItems.add(_AboutItem(about, AboutScreen.titularLabel));
+            _aboutItems.add(_AboutItem(about, AboutScreen.dioceseLabel));
+//              _aboutItems.add(_AboutItem(about, AboutScreen.historyLabel));
           }
+          _isLoading = false;
+        } else {
+          print(response.statusCode);
+          _isJsonFailed = true;
+          _isLoading = false;
         }
-      );
+      });
     }
   }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Container(
-          padding: EdgeInsets.symmetric(vertical: 50.0),
-          child: Stack(
-            alignment: Alignment.topCenter,
-            children: <Widget>[            
-              Column(
-                children: <Widget>[
-                  Container(   
-                    margin: EdgeInsets.only(top: 30.0), 
-                    height: 40.0,        
-                    decoration: BoxDecoration(
-                      color: Colors.brown[600],
-                    border: Border.all(width: 0.8),
-                    borderRadius: BorderRadius.circular(10.0),
-                  ),
-                  child: Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("Mount Carmel Church now a National Shrine",
-                    style: TextStyle(color: Colors.white,
-                    fontFamily: "Helvetica"),
-                    textAlign: TextAlign.center,
-                        ),
-                      ),
-                    ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Text("About the Church",
-                      style: AppConstants.OPTION_STYLE3,
-                      textAlign: TextAlign.center,
-                    ),
-                  ),
-
-                  Expanded(
-                      child: Container(
-                        height: 1000.0,
-                      alignment: Alignment.bottomCenter,
-                      margin: EdgeInsets.symmetric(
-                        horizontal: 50.0,
-                      ),
-                      child: ListView.builder(
-                        itemCount: _aboutItems.length,
-                        itemBuilder:(context, index){
-                          return _aboutItem(_aboutItems[index]);
-                        }
-                      ),
-                    ),
-                  ),
-
-                Container(
-                  alignment: Alignment.bottomCenter,
-                margin: EdgeInsets.only(bottom: 30.0),
-                child: leftArrowBackButton(context: context),
-                      )
-                    ],
-                  ),
-                Container(
-                  alignment: Alignment.center,
-                  child: Icon(MountCarmelIcons.logo, 
-                    color: Color.fromARGB(0x80, 0x3E, 0x27, 0x23), 
-                    size: 150.0,),
-                ),
-            ],
+      body: Container(
+        padding: EdgeInsets.symmetric(horizontal: 20.0, vertical: 50.0),
+        child: Column(
+            children: <Widget>[
+        Expanded(
+        child: SingleChildScrollView(
+            child: Column(
+            children: <Widget>[
+            Container(
+            margin: EdgeInsets.only(top: 30.0),
+        height: 40.0,
+        decoration: BoxDecoration(
+          color: Colors.brown[600],
+          border: Border.all(width: 0.0),
+          borderRadius: BorderRadius.circular(10.0),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Text(
+            "Mount Carmel Church now a National Shrine",
+            style: TextStyle(
+                color: Colors.white, fontFamily: "Helvetica"),
+            textAlign: TextAlign.center,
           ),
         ),
       ),
+      Container(
+        alignment: Alignment.center,
+        child: Image.asset("assets/images/mt_logo.png"),
+      ),
+      Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Text(
+          "About the Church",
+          style: AppConstants.OPTION_STYLE3,
+          textAlign: TextAlign.center,
+        ),
+      ),
+      ListView.builder(
+          shrinkWrap: true,
+          physics: ScrollPhysics(parent: ScrollPhysics()),
+          itemCount: _aboutItems.length,
+          itemBuilder: (context, index) {
+            return _aboutItem(_aboutItems[index]);
+          }),
+      Divider(),
+      _history(),
+      ],
+    ),
+    ),
+    ),
+    Container(
+    alignment: Alignment.bottomCenter,
+    margin: EdgeInsets.only(bottom: 30.0),
+    child: leftArrowBackButton(context: context),
+    )
+    ],
+    ),
+    ),
     );
+
+
   }
 
-  void close(){
+
+  Widget _history() {
+    if (_aboutList[0].content == null
+        || _aboutList[0].content.isEmpty)
+      return Container();
+    else {
+      return Column(children: <Widget>[
+        Text("History", style: AppConstants.OPTION_STYLE3,),
+        Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: HtmlTextView(
+            data: "<div style='color: #5d4037'>${_aboutList[0].content}</div>",
+            anchorColor: Color(0xFFFF0000),
+          ),
+        )
+      ],);
+    }
+  }
+
+  void close() {
     print("about screen close");
     this.dispose();
   }
 
-  Widget _aboutItem(_AboutItem aboutItem){    
+  Widget _aboutItem(_AboutItem aboutItem) {
     return Container(
       child: Column(
         children: <Widget>[
@@ -165,35 +177,38 @@ class _AboutScreenState extends State<AboutScreen> {
             mainAxisAlignment: MainAxisAlignment.center,
             children: <Widget>[
               Expanded(
-                child: Text("${aboutItem.label()} :",
-                      style: AppConstants.OPTION_STYLE2,
-                      textAlign: TextAlign.start,
-                    ),
+                child: Text(
+                  "${aboutItem.label()} :",
+                  style: AppConstants.OPTION_STYLE2,
+                  textAlign: TextAlign.start,
+                ),
               ),
-              SizedBox(width: 10.0,),
+              SizedBox(
+                width: 10.0,
+              ),
               Expanded(
-                child: Text("${aboutItem._value}",
-                      style: AppConstants.OPTION_STYLE2,
-                      textAlign: TextAlign.left,
-                    ),
+                child: Text(
+                  "${aboutItem._value}",
+                  style: AppConstants.OPTION_STYLE2,
+                  textAlign: TextAlign.left,
+                ),
               ),
+              Divider(),
             ],
           ),
-          lineWidget(),
         ],
       ),
     );
   }
 }
 
-class _AboutItem{
-
+class _AboutItem {
   String _label;
   String _value;
 
-  _AboutItem(About about, String label){
+  _AboutItem(About about, String label) {
     _label = label;
-    switch(label){
+    switch (label) {
       case AboutScreen.dateOfEstablishmentLabel:
         _value = about.dateOfEstablishment;
         break;
@@ -206,15 +221,17 @@ class _AboutItem{
       case AboutScreen.dioceseLabel:
         _value = about.diocese;
         break;
+      case AboutScreen.historyLabel:
+        _value = about.content;
+        break;
     }
   }
 
-  String label(){
+  String label() {
     return _label;
   }
 
-  String value(){
+  String value() {
     return _value;
   }
-
 }
