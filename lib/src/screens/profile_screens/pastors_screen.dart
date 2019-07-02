53d1df233dfad0fc349b97499a68ef03e72e606e
Adrian Evangelista <adrianevangelista.bicolresearch@gmail.com>
2019-07-02 09:39:02 +0800

    Fixed the reference of pastors for pastors screen. added failedMessage widget.

--- lib/src/screens/profile_screens/pastors_screen.dart
+++ lib/src/screens/profile_screens/pastors_screen.dart
@@ -10,6 +10,7 @@ import 'package:flutter/material.dart';
 import 'package:mt_carmel_app/src/constants/app_constants.dart';
 import 'package:mt_carmel_app/src/models/pastor.dart';
 import 'package:http/http.dart' as http;
+import 'package:mt_carmel_app/src/widgets/failed_message.dart';
 import 'dart:async';
 import 'dart:convert';
 
@@ -36,19 +37,18 @@ class _PastorsScreenState extends State<PastorsScreen> {
   }
 
   Future<void> getJasonData() async {
-    var response = await http.get(AppConstants.PRIESTS_JSON_URL);
+    var response = await http.get(AppConstants.PASTORS_JSON_URL);
     if (this.mounted) {
       setState(() {
         if (response.statusCode == 200) {
           _pastorList = (json.decode(response.body) as List)
               .map((data) => new Pastor.fromJson(data))
               .toList();
-          _isLoading = false;
         } else {
           print(response.statusCode);
           _isJsonFailed = true;
-          _isLoading = false;
         }
+        _isLoading = false;
       });
     }
   }
@@ -65,58 +65,60 @@ class _PastorsScreenState extends State<PastorsScreen> {
             Expanded(
               child: (_isLoading)
                   ? Center(child: CircularProgressIndicator())
-                  : Container(
-                      child: Column(
-                        children: <Widget>[
-                          Container(
-                            margin: EdgeInsets.only(
-                              top: 10.0,
-                            ),
-                            height: 40.0,
-                            decoration: BoxDecoration(
-                              color: Colors.brown[600],
-                              border: Border.all(width: 0.8),
-                              borderRadius: BorderRadius.circular(10.0),
-                            ),
-                            child: Padding(
-                              padding: const EdgeInsets.all(8.0),
-                              child: Text(
-                                "Mount Carmel Church now a National Shrine",
-                                style: TextStyle(
-                                    color: Colors.white,
-                                    fontFamily: "Helvetica"),
-                                textAlign: TextAlign.center,
+                  : _isJsonFailed
+                      ? failedMessage()
+                      : Container(
+                          child: Column(
+                            children: <Widget>[
+                              Container(
+                                margin: EdgeInsets.only(
+                                  top: 10.0,
+                                ),
+                                height: 40.0,
+                                decoration: BoxDecoration(
+                                  color: Colors.brown[600],
+                                  border: Border.all(width: 0.8),
+                                  borderRadius: BorderRadius.circular(10.0),
+                                ),
+                                child: Padding(
+                                  padding: const EdgeInsets.all(8.0),
+                                  child: Text(
+                                    "Mount Carmel Church now a National Shrine",
+                                    style: TextStyle(
+                                        color: Colors.white,
+                                        fontFamily: "Helvetica"),
+                                    textAlign: TextAlign.center,
+                                  ),
+                                ),
                               ),
-                            ),
-                          ),
-                          Padding(
-                            padding: const EdgeInsets.all(8.0),
-                            child: Text(
-                              "Pastors",
-                              style: AppConstants.OPTION_STYLE3,
-                              textAlign: TextAlign.center,
-                            ),
-                          ),
-                          Expanded(
-                            child: Container(
-                              margin: EdgeInsets.symmetric(
-                                horizontal: 50.0,
+                              Padding(
+                                padding: const EdgeInsets.all(8.0),
+                                child: Text(
+                                  "Pastors",
+                                  style: AppConstants.OPTION_STYLE3,
+                                  textAlign: TextAlign.center,
+                                ),
                               ),
-                              child: ListView.builder(
-                                  physics:
-                                      ScrollPhysics(parent: ScrollPhysics()),
-                                  shrinkWrap: true,
-                                  reverse: true,
-                                  itemCount: _pastorList.length,
-                                  itemBuilder: (context, index) {
-                                    return _pastorItem(
-                                        context, _pastorList[index]);
-                                  }),
-                            ),
+                              Expanded(
+                                child: Container(
+                                  margin: EdgeInsets.symmetric(
+                                    horizontal: 50.0,
+                                  ),
+                                  child: ListView.builder(
+                                      physics: ScrollPhysics(
+                                          parent: ScrollPhysics()),
+                                      shrinkWrap: true,
+                                      reverse: true,
+                                      itemCount: _pastorList.length,
+                                      itemBuilder: (context, index) {
+                                        return _pastorItem(
+                                            context, _pastorList[index]);
+                                      }),
+                                ),
+                              ),
+                            ],
                           ),
-                        ],
-                      ),
-                    ),
+                        ),
             ),
             leftArrowBackButton(context: context),
           ],
