87a9551cb2d9bea7b806efd34a2d9990f70ce789
Adrian Evangelista <adrianevangelista.bicolresearch@gmail.com>
2019-07-03 10:09:44 +0800

    Implemented caching of images.

--- lib/src/widgets/services_tiles.dart
+++ lib/src/widgets/services_tiles.dart
@@ -1,22 +1,17 @@
+/*
+*	Filename		:	services_screen.dart
+*	Purpose			:	Displays the list of the services of the church
+* Created			: 2019-06-11 15:52:50 by Detective Conan
+*	Updated			: 2019-07-03 10:08:21 by Detective conan
+*	Changes			: Implemented caching of images.
+*/
+
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
 import 'package:mt_carmel_app/src/constants/app_constants.dart';
 import 'package:mt_carmel_app/src/models/service_item.dart';
 import 'package:mt_carmel_app/src/presentations/mount_carmel_icons.dart';
 
-//Widget serviceTile(String serviceName, String description){
-//  return Padding(
-//    padding: const EdgeInsets.symmetric(vertical: 15.0),
-//    child: ListTile(title: Text(serviceName,
-//      style: AppConstants.OPTION_STYLE2),
-//      subtitle: Text('$description',
-//      style: AppConstants.OPTION_STYLE1),
-//      leading: Icon(_serviceIcon(serviceName),
-//        size: 60.0,
-//        color: Colors.brown,
-//        ),
-//    ),
-//  );
-//}
 
 Widget serviceTile(ServiceItem serviceItem) {
   return Padding(
@@ -24,9 +19,17 @@ Widget serviceTile(ServiceItem serviceItem) {
     child: Row(
       children: <Widget>[
         Container(
-            child: Image.network(
-                AppConstants.API_BASE_URL + serviceItem.coverPhoto,
-                height: 60.0)),
+          height: 60.0,
+          child: CachedNetworkImage(
+              imageUrl:
+                  AppConstants.API_BASE_URL + serviceItem.coverPhoto,
+              placeholder: (context, url) =>
+                  Center(child: CircularProgressIndicator()),
+              errorWidget: (context, url, error) => Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Icon(MountCarmelIcons.services)),
+              fit: BoxFit.cover),
+        ),
         Expanded(
           child: Padding(
             padding: const EdgeInsets.all(8.0),
@@ -35,7 +38,8 @@ Widget serviceTile(ServiceItem serviceItem) {
               crossAxisAlignment: CrossAxisAlignment.start,
               children: <Widget>[
                 Text(serviceItem.name, style: AppConstants.OPTION_STYLE2),
-                Text(serviceItem.description, style: AppConstants.OPTION_STYLE1),
+                Text(serviceItem.description,
+                    style: AppConstants.OPTION_STYLE1),
               ],
             ),
           ),
