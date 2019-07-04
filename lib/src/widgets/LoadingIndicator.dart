eea0800f477b82ef0c96a8cd946bf0774e088289
Adrian Evangelista <adrianevangelista.bicolresearch@gmail.com>
2019-07-04 08:47:20 +0800

    Changed the strokeWidth of LoadingIndicator from 4.0 to 2.0.

--- lib/src/widgets/LoadingIndicator.dart
+++ lib/src/widgets/LoadingIndicator.dart
@@ -2,8 +2,8 @@
 *  Filename    :   LoadingIndicator.dart
 *  Purpose     :	 Displays CircularProgressIndicator when loading
 *  Created     :   2019-07-03 16:40 by Detective Conan
-*  Updated     :   2019-07-03 16:40 by Detective Conan 
-*  Changes     :
+*  Updated     :   2019-07-04 08:46 by Detective conan 
+*  Changes     :   Changed the strokeWidth from 4.0 to 2.0.
 */
 
 import 'package:flutter/material.dart';
@@ -11,6 +11,9 @@ import 'package:flutter/material.dart';
 class LoadingIndicator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Center(child: CircularProgressIndicator());
+    return Center(
+        child: CircularProgressIndicator(
+      strokeWidth: 2.0,
+    ));
   }
 }
