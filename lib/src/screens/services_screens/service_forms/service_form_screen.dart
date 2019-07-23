e5a59468eacf2b05c668c5f861d8669b39882c1e
Adrian Evangelista <adrianevangelista.bicolresearch@gmail.com>
2019-07-23 10:24:22 +0800

    Added scroll notification and more arrow down and up.

--- lib/src/screens/services_screens/service_forms/service_form_screen.dart
+++ lib/src/screens/services_screens/service_forms/service_form_screen.dart
@@ -2,19 +2,20 @@
 *  Filename    :   service_form_screen.dart
 *  Purpose     :	
 *  Created     :   2019-07-15 14:12 by Detective Conan
-*  Updated     :   2019-07-22 08:54 by Detective conan
-*  Changes     :   Added padding for formFields
+*  Updated     :   2019-07-23 10:23 by Detective conan
+*  Changes     :   Added scroll notification and more arrow down and up.
 */
 
 import 'package:flutter/material.dart';
 import 'package:flutter_form_builder/flutter_form_builder.dart';
+import 'package:mt_carmel_app/src/helpers/visibility_helper.dart';
 import 'package:mt_carmel_app/src/models/church_service.dart';
 import 'package:mt_carmel_app/src/screens/services_screens/service_forms/service_form_field.dart';
 import 'package:mt_carmel_app/src/screens/services_screens/thank_you/thank_you_screen.dart';
 import 'package:mt_carmel_app/src/widgets/left_arrow_back_button.dart';
 import 'package:mt_carmel_app/src/widgets/line.dart';
 
-class ServiceFormScreen extends StatelessWidget {
+class ServiceFormScreen extends StatefulWidget {
   static final GlobalKey<FormBuilderState> _fbKey =
       GlobalKey<FormBuilderState>();
 
@@ -22,6 +23,52 @@ class ServiceFormScreen extends StatelessWidget {
 
   @required
   final ChurchServiceSubtype serviceSubType;
+
+  @override
+  _ServiceFormScreenState createState() => _ServiceFormScreenState();
+}
+
+class _ServiceFormScreenState extends State<ServiceFormScreen> {
+  bool _isScrolledToTheLast = false;
+
+  ScrollController _scrollController;
+
+  static Icon _arrowUp = Icon(
+    Icons.keyboard_arrow_up,
+    color: Colors.brown,
+  );
+  static Icon _arrowDown = Icon(
+    Icons.keyboard_arrow_down,
+    color: Colors.brown,
+  );
+
+  VisibilityHelper _arrowMoreUp = VisibilityHelper(
+    child: _arrowUp,
+    visibility: VisibilityFlag.gone,
+  );
+  VisibilityHelper _arrowMoreDown = VisibilityHelper(
+    child: _arrowDown,
+    visibility: VisibilityFlag.gone,
+  );
+
+  Future _initializeArrows() async {
+    await Future.delayed(Duration(milliseconds: 800));
+    _scrollListener();
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  @override
+  void initState() {
+    _scrollController = ScrollController();
+    _scrollController.addListener(_scrollListener);
+    super.initState();
+    _initializeArrows();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -34,7 +81,7 @@ class ServiceFormScreen extends StatelessWidget {
             Container(
               padding: const EdgeInsets.symmetric(vertical: 30.0),
               child: Text(
-                serviceSubType.subTypeName,
+                widget.serviceSubType.subTypeName,
                 style: Theme.of(context)
                     .primaryTextTheme
                     .headline
@@ -43,30 +90,42 @@ class ServiceFormScreen extends StatelessWidget {
               ),
             ),
             lineWidget(),
-            SizedBox(
-              height: 10.0,
-            ),
+            GestureDetector(onTap: _moveUp, child: _arrowMoreUp),
             Expanded(
-              child: SingleChildScrollView(
-                child: Column(
-                  children: <Widget>[
-                    Padding(
-                      padding: const EdgeInsets.symmetric(horizontal: 12.0),
-                      child: FormBuilder(
-                        key: _fbKey,
-                        autovalidate: true,
-                        child: Column(
-                          children: <Widget>[
-                            for (var formField in serviceSubType.formFields)
-                              ServiceFormField(churchFormField: formField),
-                          ],
+              child: NotificationListener<ScrollNotification>(
+                onNotification: (scrollNotification) {
+                  if (scrollNotification is ScrollStartNotification) {
+                    _onStartScroll(scrollNotification.metrics);
+                  } else if (scrollNotification is ScrollUpdateNotification) {
+                    _onUpdateScroll(scrollNotification.metrics);
+                  } else if (scrollNotification is ScrollEndNotification) {
+                    _onEndScroll(scrollNotification.metrics);
+                  }
+                  return;
+                },
+                child: SingleChildScrollView(
+                  controller: _scrollController,
+                  child: Column(
+                    children: <Widget>[
+                      Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 12.0),
+                        child: FormBuilder(
+                          key: ServiceFormScreen._fbKey,
+                          autovalidate: true,
+                          child: Column(
+                            children: <Widget>[
+                              for (var formField in widget.serviceSubType.formFields)
+                                ServiceFormField(churchFormField: formField),
+                            ],
+                          ),
                         ),
                       ),
-                    ),
-                  ],
+                    ],
+                  ),
                 ),
               ),
             ),
+            GestureDetector(onTap: _moveDown, child: _arrowMoreDown),
             RaisedButton(
               shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(20.0)),
@@ -75,17 +134,18 @@ class ServiceFormScreen extends StatelessWidget {
                 "Submit",
                 style: TextStyle(color: Colors.white),
               ),
-              onPressed: () async {
+              onPressed: _isScrolledToTheLast?() async {
                 //TODO implement validations and updating database
                 final result = await Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (context) => ThankYouScreen(
-                        thankYouText: serviceSubType.thankYouText),
+                        thankYouText: widget.serviceSubType.thankYouText),
                   ),
                 );
                 if (result) Navigator.pop(context, true);
-              },
+              }
+              :null,
             ),
             leftArrowBackButton(context: context),
           ],
@@ -93,4 +153,84 @@ class ServiceFormScreen extends StatelessWidget {
       ),
     );
   }
+
+  void _onStartScroll(ScrollMetrics metrics) {}
+
+  void _onUpdateScroll(ScrollMetrics metrics) {}
+
+  void _onEndScroll(ScrollMetrics metrics) {
+    _scrollListener();
+  }
+
+  _scrollListener() {
+    if (!_scrollController.hasClients) return;
+    try {
+      if (_scrollController.offset ==
+          _scrollController.position.maxScrollExtent &&
+          _scrollController.offset ==
+              _scrollController.position.minScrollExtent) {
+        setState(() {
+          _arrowMoreDown = VisibilityHelper(
+              child: _arrowDown, visibility: VisibilityFlag.gone);
+          _arrowMoreUp = VisibilityHelper(
+              child: _arrowUp, visibility: VisibilityFlag.gone);
+          _isScrolledToTheLast = true;
+        });
+        return;
+      }
+      if (_scrollController.offset >=
+          _scrollController.position.maxScrollExtent &&
+          !_scrollController.position.outOfRange) {
+        setState(
+              () {
+            _arrowMoreDown = VisibilityHelper(
+                child: _arrowDown, visibility: VisibilityFlag.gone);
+            _arrowMoreUp = VisibilityHelper(
+                child: _arrowUp, visibility: VisibilityFlag.visible);
+            _isScrolledToTheLast = true;
+          },
+        );
+        return;
+      }
+      if (_scrollController.offset <=
+          _scrollController.position.minScrollExtent &&
+          !_scrollController.position.outOfRange) {
+        setState(() {
+          _arrowMoreDown = VisibilityHelper(
+              child: _arrowDown, visibility: VisibilityFlag.visible);
+          _arrowMoreUp = VisibilityHelper(
+              child: _arrowUp, visibility: VisibilityFlag.gone);
+        });
+        return;
+      }
+      if (_scrollController.offset <
+          _scrollController.position.maxScrollExtent &&
+          _scrollController.offset >
+              _scrollController.position.minScrollExtent) {
+        setState(() {
+          _arrowMoreDown = VisibilityHelper(
+              child: _arrowDown, visibility: VisibilityFlag.visible);
+          _arrowMoreUp = VisibilityHelper(
+              child: _arrowUp, visibility: VisibilityFlag.visible);
+        });
+        return;
+      }
+    } catch (e) {
+      print(e.toString());
+      print("error in _scrolListener");
+    }
+  }
+
+  _moveUp() {
+    if (_scrollController.offset >= _scrollController.position.minScrollExtent)
+      _scrollController.animateTo(_scrollController.offset - 200,
+          curve: Curves.linear, duration: Duration(milliseconds: 500));
+  }
+
+  _moveDown() {
+    if (_scrollController.offset <= _scrollController.position.maxScrollExtent)
+      _scrollController.animateTo(_scrollController.offset + 200,
+          curve: Curves.linear, duration: Duration(milliseconds: 500));
+  }
+
 }
