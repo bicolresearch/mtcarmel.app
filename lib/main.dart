225c3abf412dd67ccedd8bd17ecd19de6fd22eae
Adrian Evangelista <adriane.macer@gmail.com>
2019-09-09 12:49:13 +0800

    Update main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,8 @@
 *  Filename    :   main.dart
 *  Purpose     :   Entry point of app.
 *  Created     :   2019-07-12 16:44 by Detective Conan
-*  Updated     :   2019-08-20 11:27 by Detective conan
-*	 Updated			:   04/09/2019 1:33 PM PM by Detective Conan
-*	 Changes			:   Implemented Bloc from loading branch and displaying tabs.
+*	 Updated			:   09/09/2019 12:46 PM PM by Detective Conan
+*	 Changes			:   Route to StartPage instead to Page.
 */
 
 import 'dart:async';
@@ -15,16 +14,20 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:mt_carmel_app/src/blocs/branch_bloc/branch_bloc.dart';
 import 'package:mt_carmel_app/src/blocs/branch_bloc/branch_event.dart';
+import 'package:mt_carmel_app/src/blocs/branch_selection_bloc/branch_selection_bloc.dart';
+import 'package:mt_carmel_app/src/blocs/branch_selection_bloc/branch_selection_event.dart';
 import 'package:mt_carmel_app/src/blocs/concrete_bloc_delegate.dart';
 import 'package:mt_carmel_app/src/constants/app_constants.dart';
 import 'package:mt_carmel_app/src/core/services/service_locator.dart';
 import 'package:mt_carmel_app/src/repositories/branches_repo.dart';
+import 'package:mt_carmel_app/src/screens/branch_selection_screen.dart';
 import 'package:mt_carmel_app/src/screens/home_screen.dart';
+import 'package:mt_carmel_app/src/screens/start_page.dart';
 import 'package:mt_carmel_app/src/screens/tab_selector.dart';
 import 'package:mt_carmel_app/src/screens/edit_profile_screen.dart';
 import 'package:mt_carmel_app/src/utils/development_production_enum.dart';
 import 'package:mt_carmel_app/src/widgets/app_theme_data.dart';
-import 'src/screens/splash.dart';
+import 'src/screens/splash_screen.dart';
 import 'src/screens/introduction_screen.dart';
 import 'package:mt_carmel_app/src/helpers/shared_preference_helper.dart';
 import 'package:intl/date_symbol_data_local.dart';
@@ -47,12 +50,15 @@ void main() {
 class MtCarmelApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: AppConstants.APP_TITLE,
-      debugShowCheckedModeBanner: false,
-      theme: appThemeData(context),
-      home: Page(
+    BlocSupervisor.delegate = ConcreteBlocDelegate();
+    return BlocProvider(
+      builder: (context) =>
+          BranchSelectionBloc()..dispatch(BranchSelectionFetch()),
+      child: MaterialApp(
         title: AppConstants.APP_TITLE,
+        debugShowCheckedModeBanner: false,
+        theme: appThemeData(context),
+        home: StartPage(),
       ),
     );
   }
@@ -125,11 +131,9 @@ class _PageState extends State<Page> {
     if (isFirstUsage) {
       return IntroScreen();
     }
-    BlocSupervisor.delegate = ConcreteBlocDelegate();
     final branchesRepo = BranchesRepo();
     return BlocProvider(
-      builder: (builder) =>
-          BranchBloc(branchesRepo: branchesRepo)..dispatch(GetBranch("1")),
+      builder: (builder) => BranchBloc()..dispatch(GetBranch("1")),
       child: HomeScreen(),
     );
   }
