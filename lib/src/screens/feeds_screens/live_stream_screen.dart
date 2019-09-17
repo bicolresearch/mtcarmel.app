90c216be14152948613a7bc14d787530769c9df4
Adrian Evangelista <adriane.macer@gmail.com>
2019-09-17 17:59:12 +0800

    Placed arrow back button for every state of live stream sceen

--- lib/src/screens/feeds_screens/live_stream_screen.dart
+++ lib/src/screens/feeds_screens/live_stream_screen.dart
@@ -2,8 +2,8 @@
 *   Filename    :   live_stream_screen.dart
 *   Purpose     :
 *   Created     :   10/09/2019 5:01 PM by Detective Conan
-*   Updated     :   10/09/2019 5:01 PM by Detective Conan
-*   Changes     :   
+*	 Updated			:   17/09/2019 5:58 PM PM by Detective Conan
+*	 Changes			:   Placed arrow back button for every state
 */
 
 import 'package:flutter/material.dart';
@@ -15,40 +15,46 @@ import 'package:mt_carmel_app/src/screens/feeds_screens/youtube_player_screen.da
 import 'package:mt_carmel_app/src/widgets/error_message.dart';
 import 'package:mt_carmel_app/src/widgets/left_arrow_back_button.dart';
 import 'package:mt_carmel_app/src/widgets/loading_indicator.dart';
-import 'package:provider/provider.dart';
 
 class LiveStreamScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: BlocBuilder<LiveStreamBloc, LiveStreamState>(
-          builder: (context, state) {
-        if (state is LiveStreamUninitialized || state is LiveStreamLoading) {
-          return Column(
-            children: <Widget>[
-              Expanded(child: Center(child: LoadingIndicator())),
-              leftArrowBackButton(context: context),
-              SizedBox(
-                height: 20,
-              )
-            ],
-          );
-        }
-        if (state is LiveStreamLoaded) {
-          final LiveStream liveStream = state.liveStream;
-          final String videoId =
-              (liveStream.data != null && liveStream.data.isNotEmpty)
-                  ? liveStream.data[0].videoId
-                  : null;
-          if (videoId == null || videoId.isEmpty)
-            return ErrorMessage.errMsg(errorMessage: "Something Went wrong");
-          return YoutubePlayerScreen(videoId: videoId);
-        }
-        if (state is LiveStreamError) {
-          return ErrorMessage.errMsg(errorMessage: "Something Went wrong");
-        }
-        return Container();
-      }),
+      body: Column(
+        children: [
+          Expanded(
+            child: BlocBuilder<LiveStreamBloc, LiveStreamState>(
+                builder: (context, state) {
+              if (state is LiveStreamUninitialized ||
+                  state is LiveStreamLoading) {
+                return Center(
+                  child: LoadingIndicator(),
+                );
+              }
+              if (state is LiveStreamLoaded) {
+                final LiveStream liveStream = state.liveStream;
+                final String videoId =
+                    (liveStream.data != null && liveStream.data.isNotEmpty)
+                        ? liveStream.data[0].videoId
+                        : null;
+                if (videoId == null || videoId.isEmpty)
+                  return ErrorMessage.errMsg(
+                      errorMessage: "Something Went wrong");
+                return YoutubePlayerScreen(videoId: videoId);
+              }
+              if (state is LiveStreamError) {
+                return ErrorMessage.errMsg(
+                    errorMessage: "Something Went wrong");
+              }
+              return Container();
+            }),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20.0),
+            child: leftArrowBackButton(context: context),
+          ),
+        ],
+      ),
     );
   }
 }
