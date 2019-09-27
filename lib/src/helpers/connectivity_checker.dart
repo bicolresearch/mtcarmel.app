bea43472d4cc7490e1392925d07432e3ad05639a
Adrian Evangelista <adriane.macer@gmail.com>
2019-09-27 13:41:42 +0800

    Replaced the package connectivity. Added data_connection_checker package.

--- lib/src/helpers/connectivity_checker.dart
+++ lib/src/helpers/connectivity_checker.dart
@@ -6,17 +6,19 @@
 *  Changes     :
 */
 
-import 'package:connectivity/connectivity.dart';
+//import 'package:connectivity/connectivity.dart';
+import 'package:data_connection_checker/data_connection_checker.dart';
 
 class ConnectivityChecker {
   static Future<bool> hasDataConnection() async {
-    bool connected = false;
-    final connectivity = await (Connectivity().checkConnectivity())
-        .timeout(Duration(seconds: 10));
-    if (connectivity == ConnectivityResult.mobile ||
-        connectivity == ConnectivityResult.wifi) {
-      connected = true;
-    }
+//    bool connected = false;
+//    final connectivity = await (Connectivity().checkConnectivity())
+//        .timeout(Duration(seconds: 10));
+//    if (connectivity == ConnectivityResult.mobile ||
+//        connectivity == ConnectivityResult.wifi) {
+//      connected = true;
+//    }
+    bool connected = await DataConnectionChecker().hasConnection;
     return connected;
   }
 }
